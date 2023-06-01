2671e241003e89daab376c9fe9a4cd9f404608ec
Aminulhoque01 <aminuhoque53@gmail.com>
2023-06-02 03:00:38 +0600

    [Done]setup user interface

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-// import usersRouter from './app/modules/users/user.route'
+import usersRouter from './app/modules/user/user.route'
 
 const app: Application = express()
 
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 //Application routes
+app.use('/api/v1/users', usersRouter)
 
 // app.use('/api/v1/users', usersRouter)
 
